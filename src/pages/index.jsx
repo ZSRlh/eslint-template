dd0ed15a7a913128648d45c537aa3aba40b7d0fa
ZSRlh <952177238@qq.com>
2021-10-09 17:54:06 +0800

    feat: eslint config

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,11 +1,10 @@
-import React from 'react';
+import React from "reacr";
 
-export default function Home () {
+export default function Home() {
   let a = 1;
-  return (
-    <div>
-      123
-    </div>
-  )
+  return <div>123</div>;
 }
-
+let a = [
+  2, 2, 2, 2, 2, 2, 2, 288888, 88888888, 2, 2, 2, 2, 2, 2, 9, 9, 9, 999999, 8897, 9, 987, 9, 8,
+  8989, 3, 4, 4, 5, 6, 7, 8, 777,
+];
